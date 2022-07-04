b6782010165972f07586ab1300b49bd8e402cbe1
ganeshprasadbhandari <ganeshprasadbhandari79@gmail.com>
2022-07-04 14:56:12 +0530

    updating changes

--- init_setup.sh
+++ init_setup.sh
@@ -1,11 +1,20 @@
+# echo [$(date)]: "START"
+# echo [$(date)]: "creating environment"
+# conda create --prefix ./env python=3.7 -y
+# echo [$(date)]: "activate environment"
+# source activate ./env
+# echo [$(date)]: "install requirements"
+# pip install -r requirements.txt
+# echo [$(date)]: "END"
+
+# # to remove everything -
+# # rm -rf env/ .gitignore conda.yaml README.md .git/
+
 echo [$(date)]: "START"
 echo [$(date)]: "creating environment"
-conda create --prefix ./env python=3.8 -y
+conda create --p ./env python=3.8 -y
 echo [$(date)]: "activate environment"
-source activate ./env
+conda activate ./env/
 echo [$(date)]: "install requirements"
 pip install -r requirements.txt
 echo [$(date)]: "END"
-
-# to remove everything -
-# rm -rf env/ .gitignore conda.yaml README.md .git/
